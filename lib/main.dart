d9c7732e7e9ff2b54e905ab6b94c4b260279599c
KMtouzhele <kilmerau@hotmail.com>
2024-05-24 15:32:17 +1000

    firestore configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("\n\nConnected to Firebase App ${app.options.projectId}\n\n");
+
   runApp(const MyApp());
 }
 
